e3919923353578b060c928965653e21054cecef5
Jatin Srivastava <jatinsrivastava135@gmail.com>
2025-11-30 00:42:03 +0530

    Modification done to silver tables

--- scripts/silver/06_silver_data_load.sql
+++ scripts/silver/06_silver_data_load.sql
@@ -3,6 +3,7 @@
    ==============================
 */
 
+
 INSERT INTO silver.crm_cust_info(
 	cst_id,
 	cst_key,
@@ -37,4 +38,42 @@ FROM (
 )t
 WHERE t.r_num = 1 -- Select the most recent records per customer
 
-SELECT * FROM silver.crm_cust_info
+
+-- SELECT * FROM silver.crm_cust_info
+
+
+/* ==============================
+	Table: crm_cust_info
+   ==============================
+*/
+
+
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract Category ID
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Extract Product Key
+	TRIM(prd_nm) AS prd_nm,
+	COALESCE(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Roads'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'Unknown'
+	END AS prd_line, -- Map Product Line codes to descriptive values
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt -- Calculate end date as one day before the next start date
+FROM bronze.crm_prd_info
+
+
+-- SELECT * FROM silver.crm_prd_info
--- scripts/silver/07_silver_data_profiling.sql
+++ scripts/silver/07_silver_data_profiling.sql
@@ -4,20 +4,32 @@
 	--> Check the consistency of low cardinality columns
 */
 
+-- NOTE: BELOW PROCESS DONE FOR EVERY TABLE IN BRONZE LAYER
+
 -- Check for Duplicates and NULL Values
 SELECT 
-	cst_id,
+	prd_id,
 	COUNT(*)
-FROM bronze.crm_cust_info
-GROUP BY cst_id
-HAVING COUNT(*) > 1 OR cst_id IS NULL
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
 
 -- Check for Extra Spaces
-SELECT cst_firstname
-FROM bronze.crm_cust_info
-WHERE cst_firstname != TRIM(cst_firstname)
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
 
 -- Check for Data Standardization and Consistency
 SELECT 
-	DISTINCT cst_gndr
-FROM bronze.crm_cust_info
+	DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+-- Check for NULLS or Negative Numbers
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_cost IS NULL OR prd_cost < 0
+
+-- Check for Invalid Date Orders 
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
