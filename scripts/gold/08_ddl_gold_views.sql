4501a45eca65d557a80cb64dd265367365556a12
Jatin Srivastava <jatinsrivastava135@gmail.com>
2025-12-04 00:14:30 +0530

    Updates to gold view query

--- scripts/gold/08_ddl_gold_views.sql
+++ scripts/gold/08_ddl_gold_views.sql
@@ -38,3 +38,31 @@ CREATE VIEW gold.dim_customers AS (
 	LEFT JOIN silver.erp_loc_a101 AS la
 		ON ci.cst_key = la.cid
 )
+
+
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+	DROP VIEW gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products AS (
+	SELECT 
+		ROW_NUMBER() OVER(ORDER BY pin.prd_start_dt, pin.prd_key) AS product_key,
+		pin.prd_id			 AS product_id,
+		pin.prd_key			 AS product_number,
+		pin.prd_nm			 AS product_name,
+		pin.cat_id			 AS category_id,
+		pc.cat				 AS category,
+		pc.subcat			 AS subcategory,
+		pc.maintenance,
+		pin.prd_cost		 AS cost,
+		pin.prd_line		 AS product_line,
+		pin.prd_start_dt	 AS start_date
+	FROM silver.crm_prd_info AS pin
+	LEFT JOIN silver.erp_px_cat_g1v2 AS pc
+		ON pin.cat_id = pc.id
+	WHERE pin.prd_end_dt IS NULL -- Filter out all historical data
+)
