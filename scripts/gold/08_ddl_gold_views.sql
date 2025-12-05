63b9db817aafc884f1c5429a76e0e00802dc71eb
Jatin Srivastava <jatinsrivastava135@gmail.com>
2025-12-05 22:52:33 +0530

    Add data model diagram also update to gold views query

--- scripts/gold/08_ddl_gold_views.sql
+++ scripts/gold/08_ddl_gold_views.sql
@@ -66,3 +66,30 @@ CREATE VIEW gold.dim_products AS (
 		ON pin.cat_id = pc.id
 	WHERE pin.prd_end_dt IS NULL -- Filter out all historical data
 )
+
+
+-- =============================================================================
+-- Create Fact: gold.fact_sales
+-- =============================================================================
+
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+	DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS (
+	SELECT 
+		sd.sls_ord_num		AS order_number,
+		p.product_key,
+		c.customer_key,
+		sd.sls_order_dt		AS order_date,
+		sd.sls_ship_dt		AS shipping_date,
+		sd.sls_due_dt		AS due_date,
+		sd.sls_sales		AS sales_amount,
+		sd.sls_quantity		AS quantity,
+		sd.sls_price		AS price
+	FROM silver.crm_sales_details AS sd
+	LEFT JOIN gold.dim_customers AS c
+		ON sd.sls_cust_id = c.customer_id
+	LEFT JOIN gold.dim_products AS p
+		ON sd.sls_prd_key =  p.product_number
+)
